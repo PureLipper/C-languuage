8935f8ba364b5da2db860d43fd9578db75e54966
PureLipper <1090527382@qq.com>
2022-11-04 18:31:52 +0800

    约瑟夫问题√

---
+++ c++ codes/P1996 约瑟夫问题.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+struct node{
+	int num;
+	node* next = NULL;
+};
+
+int main(){
+	int n,m;
+	cin >> n >> m;
+	node* head = new node;
+	node* tail = head;
+	head->num = 1;
+	head->next = NULL;
+	for(int i = 2;i <= n;i++){
+		node* temp = new node;
+		temp->num = i;
+		temp->next = NULL;
+		tail->next = temp;
+		tail = temp;
+	}
+	tail->num = n;
+	tail->next = head;
+	
+	node* curNode = tail;
+	for(int j = 1;j <= n;j++){
+		for(int i = 1;i < m;i++){
+			curNode = curNode->next;
+		}
+		cout << curNode->next->num << " ";
+		curNode->next = curNode->next->next;
+	}
+	
+}
